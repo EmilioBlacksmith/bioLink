69d4a1acb6085849972ebc19137c0269dd2fb8b6
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-09-18 20:48:47 -0600

    add main links buttons

--- bioLink/src/App.jsx
+++ bioLink/src/App.jsx
@@ -6,10 +6,15 @@ import { Profile } from './Profile';
 function App() {
 
   return (
-    <div className="h-screen flex justify-center flex-col space-y-8 items-center bg-slate-400 dark:bg-slate-800"> 
-      <Profile />
-      <DarkModeToggle />
-      <Links/>
+    <div className="h-screen flex justify-center items-center flex-col space-y-24 bg-slate-500 dark:bg-slate-800"> 
+      <div className='h-3/4 max-w-screen-2xl w-11/12 flex-col space-y-6 flex justify-center items-center'>
+        <Profile />
+        <DarkModeToggle />
+        <Links />
+      </div>
+      <div className='text-black dark:text-white'>
+        <p>Developed by <a href='https://github.com/EmilioBlacksmith'><b>Emilio Blacksmith</b></a></p>
+      </div>
     </div>
   )
 }
--- bioLink/src/Links.jsx
+++ bioLink/src/Links.jsx
@@ -2,7 +2,41 @@ export function Links()
 {
     return(
         <>
-            Kill me
+            <a 
+                className='bg-slate-300 w-1/2 py-6 rounded justify-center flex hover:shadow-lg hover:bg-slate-400
+                    dark:hover:text-white text-black dark:bg-slate-900 dark:text-white dark:hover:bg-slate-900'
+                href="https://www.youtube.com/emilioblacksmith" target="_blank"
+            >
+                My Dev Channel
+            </a>
+            <a 
+                className='bg-slate-300 w-1/2 py-6 rounded justify-center flex hover:shadow-lg hover:bg-slate-400
+                dark:hover:text-white text-black dark:bg-slate-900 dark:text-white dark:hover:bg-slate-900'
+                href="https://emilioblacksmith.itch.io/" target="_blank"
+            >
+                My Itch.io Page
+            </a>
+            <a 
+                className='bg-slate-300 w-1/2 py-6 rounded justify-center flex hover:shadow-lg hover:bg-slate-400
+                dark:hover:text-white text-black dark:bg-slate-900 dark:text-white dark:hover:bg-slate-900'
+                href="https://emilioblacksmith.github.io/" target="_blank"
+            >
+                My Website
+            </a>
+            <a 
+                className='bg-slate-300 w-1/2 py-6 rounded justify-center flex hover:shadow-lg hover:bg-slate-400
+                    dark:hover:text-white text-black dark:bg-slate-900 dark:text-white dark:hover:bg-slate-900'
+                href="https://emilioblacksmith.github.io/Blog/" target="_blank"
+            >
+                My Personal Blog
+            </a>
+            <a 
+                className='bg-slate-300 w-1/2 py-6 rounded justify-center flex hover:shadow-lg hover:bg-slate-400
+                    dark:hover:text-white text-black dark:bg-slate-900 dark:text-white dark:hover:bg-slate-900'
+                href="https://github.com/EmilioBlacksmith" target="_blank"
+            >
+                My Github
+            </a>
         </>
     )
 }
