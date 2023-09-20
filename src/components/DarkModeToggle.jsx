d85d12edaf3a3ef2723485cde3dd1a6c06080349
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-09-19 23:16:06 -0600

    finished biolinks and styles || tested

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -2,19 +2,37 @@ import { useEffect, useState } from 'react'
 import '../styles/darkModeToggle.css'
 
 export function DarkModeToggle(){
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(() =>{
+        let selectedTheme = localStorage.getItem("selectedTheme");
+
+        if(selectedTheme !== null){
+            return selectedTheme;
+        }
+
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            return 'dark';
+        }
+
+        return 'light';
+    });
+
+    const handleDarkMode = (e) => {
+        if(e.target.checked){
+            setTheme('dark');
+            localStorage.setItem("selectedTheme", 'dark')
+        }
+        else{
+            setTheme('light');
+            localStorage.setItem("selectedTheme", 'light')
+        }
+    }
 
     useEffect(() =>{
         if(theme === 'dark')
-        document.querySelector('html').setAttribute("data-theme", 'dark');
+            document.querySelector('html').setAttribute("data-theme", 'dark');
         else
-        document.querySelector('html').setAttribute("data-theme", 'light');
-    }, [theme])
-
-    const handleDarkMode = (e) => {
-        if(e.target.checked) setTheme('dark');
-        else setTheme('light');
-    }
+            document.querySelector('html').setAttribute("data-theme", 'light');
+    }, [theme])  
 
     return (
         <>
@@ -23,6 +41,7 @@ export function DarkModeToggle(){
             type='checkbox'
             id='darkmode-toggle'
             onChange={handleDarkMode}
+            defaultChecked={theme === 'dark'}
         >   
         </input>
         </>
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,33 +1,35 @@
+import '../styles/links.css'
+
 export function Links()
 {
     return(
         <div className="links">
             <a 
-                className=''
+                className='button'
                 href="https://www.youtube.com/emilioblacksmith" target="_blank"
             >
                 My Dev Channel
             </a>
             <a 
-                className=''
+                className='button'
                 href="https://emilioblacksmith.itch.io/" target="_blank"
             >
                 My Itch.io Page
             </a>
             <a 
-                className=''
+                className='button'
                 href="https://emilioblacksmith.github.io/" target="_blank"
             >
                 My Website
             </a>
             <a 
-                className=''
+                className='button'
                 href="https://emilioblacksmith.github.io/Blog/" target="_blank"
             >
                 My Personal Blog
             </a>
             <a 
-                className=''
+                className='button'
                 href="https://github.com/EmilioBlacksmith" target="_blank"
             >
                 My Github
