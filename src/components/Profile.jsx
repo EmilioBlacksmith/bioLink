ffe15b84feb21c44c8e7d26d5a52a52a9521a181
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-09 22:56:35 -0600

    update font || imgs resolution and format || update color scheme

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,15 +3,15 @@ import '../styles/profile.css';
 export function Profile(){
     return(
         <div className="profile">
-            <img src='./assets/profile.jpg'></img>
+            <img src='./assets/profile.webp'></img>
             <div className='name'>Emilio (Blacksmith) Herrera</div>
             <div className='description'>I make dev fun to watch (sometimes...)</div>
             <div className="social-media">
-                <a href='https://twitter.com/EmilioBlacksmit' target='_blank'><i className="fa-brands fa-x-twitter"/></a>
-                <a href='https://www.instagram.com/emilioblacksmith/' target='_blank'><i className="fa-brands fa-instagram"/></a>
-                <a href='https://www.youtube.com/emilioblacksmith' target='_blank'><i className="fa-brands fa-youtube"/></a>
-                <a href='https://www.twitch.tv/emilioblacksmith' target='_blank'><i className="fa-brands fa-twitch"/></a>
-                <a href='mailto:emilioblacksmithlush@gmail.com' target='_blank'><i className="fa-solid fa-envelope"/></a>
+                <a href='https://twitter.com/EmilioBlacksmit' target='_blank'></a>
+                <a href='https://www.instagram.com/emilioblacksmith/' target='_blank'></a>
+                <a href='https://www.youtube.com/emilioblacksmith' target='_blank'>󰗃</a>
+                <a href='https://www.twitch.tv/emilioblacksmith' target='_blank'></a>
+                <a href='mailto:emilioblacksmithlush@gmail.com' target='_blank'></a>
             </div>
         </div>
     )
