45ef4b23d7ff433f21f946d85cfb8663a8887930
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-10 16:18:45 -0600

    add github icon to copyright

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Links />
       </div>
       <div className='footer'>
-        <p>Developed by <a href='https://github.com/EmilioBlacksmith' target='_blank'><b>Emilio (Blacksmith) Herrera</b></a>. </p>
+        <p>Developed by <a href='https://github.com/EmilioBlacksmith' target='_blank'><b>Emilio (Blacksmith) Herrera 󰊤</b></a>. </p>
       </div>
     </div>
   )
