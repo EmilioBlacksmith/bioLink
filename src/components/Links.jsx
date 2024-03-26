e3decfb8fc364d4ea78da1ade7fcd282b6140b83
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2024-03-26 16:03:17 -0600

    update blog links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,39 +1,43 @@
-import '../styles/links.css'
+import "../styles/links.css";
 
-export function Links()
-{
-    return(
-        <div className="links">
-            <a 
-                className='button'
-                href="https://www.youtube.com/emilioblacksmith" target="_blank"
-            >
-                My Dev Channel
-            </a>
-            <a 
-                className='button'
-                href="https://emilioblacksmith.itch.io/" target="_blank"
-            >
-                My Itch.io Page
-            </a>
-            <a 
-                className='button'
-                href="https://emilioblacksmith.github.io/" target="_blank"
-            >
-                My Website
-            </a>
-            <a 
-                className='button'
-                href="https://emilioblacksmith.github.io/Blog/" target="_blank"
-            >
-                My Personal Blog
-            </a>
-            <a 
-                className='button'
-                href="https://github.com/EmilioBlacksmith" target="_blank"
-            >
-                My Github
-            </a>
-        </div>
-    )
+export function Links() {
+	return (
+		<div className="links">
+			<a
+				className="button"
+				href="https://www.youtube.com/emilioblacksmith"
+				target="_blank"
+			>
+				My Dev Channel
+			</a>
+			<a
+				className="button"
+				href="https://emilioblacksmith.itch.io/"
+				target="_blank"
+			>
+				My Itch.io Page
+			</a>
+			<a
+				className="button"
+				href="https://emilioblacksmith.github.io/"
+				target="_blank"
+			>
+				My Website
+			</a>
+			<a
+				className="button"
+				href="https://emilioblacksmith.github.io/TheBlacksmithBlog/"
+				target="_blank"
+			>
+				My Personal Blog
+			</a>
+			<a
+				className="button"
+				href="https://github.com/EmilioBlacksmith"
+				target="_blank"
+			>
+				My Github
+			</a>
+		</div>
+	);
 }
