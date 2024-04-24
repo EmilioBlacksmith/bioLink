57a640e064cbda4761cecdfa2157dc3431863144
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2024-04-24 14:42:40 -0600

    change color scheme and remove dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { DarkModeToggle } from './components/DarkModeToggle';
-import './index.css'
-import { Links } from './components/Links';
-import { Profile } from './components/Profile';
+import "./index.css";
+import { Links } from "./components/Links";
+import { Profile } from "./components/Profile";
 
 function App() {
-
-  return (
-    <div className='container'> 
-      <div className='header'>
-        <DarkModeToggle />
-      </div>
-      <div className='content'>
-        <Profile />
-        <Links />
-      </div>
-      <div className='footer'>
-        <p>Developed by <a href='https://github.com/EmilioBlacksmith/bioLink' target='_blank'><b>Emilio (Blacksmith) Herrera 󰊤</b></a>. </p>
-      </div>
-    </div>
-  )
+	return (
+		<div className="container">
+			<div className="content">
+				<Profile />
+				<Links />
+			</div>
+			<div className="footer">
+				<p>
+					Developed by{" "}
+					<a
+						href="https://github.com/EmilioBlacksmith/bioLink"
+						target="_blank"
+					>
+						<b>Emilio (Blacksmith) Herrera 󰊤</b>
+					</a>
+					.{" "}
+				</p>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,48 @@
-import '../styles/profile.css';
+import "../styles/profile.css";
 
-export function Profile(){
-    return(
-        <div className="profile">
-            <img src='./assets/images/alpha_profile.webp' alt="Emilio Blacksmith's Profile Picture"></img>
-            <div className='name'>Emilio (Blacksmith) Herrera</div>
-            <div className='description'>I make dev fun to watch (sometimes...)</div>
-            <div className="social-media">
-                <a href='https://twitter.com/EmilioBlacksmit' target='_blank'></a>
-                <a href='https://www.instagram.com/emilioblacksmith/' target='_blank'></a>
-                <a href='https://www.youtube.com/emilioblacksmith' target='_blank'>󰗃</a>
-                <a href='https://www.twitch.tv/emilioblacksmith' target='_blank'></a>
-                <a href='mailto:emilioblacksmithlush@gmail.com' target='_blank'></a>
-            </div>
-        </div>
-    )
+export function Profile() {
+	return (
+		<div className="profile">
+			<a href="/bioLink/">
+				<img
+					src="https://pbs.twimg.com/profile_images/1781539219384545280/VL8ZsI0O_400x400.jpg"
+					alt="Emilio Blacksmith's Profile Picture"
+				></img>
+			</a>
+			<div className="name">Emilio (Blacksmith) Herrera</div>
+			<div className="description">I make dev fun to watch (sometimes...)</div>
+			<div className="social-media">
+				<a
+					href="https://twitter.com/EmilioBlacksmit"
+					target="_blank"
+				>
+					
+				</a>
+				<a
+					href="https://www.instagram.com/emilioblacksmith/"
+					target="_blank"
+				>
+					
+				</a>
+				<a
+					href="https://www.youtube.com/emilioblacksmith"
+					target="_blank"
+				>
+					󰗃
+				</a>
+				<a
+					href="https://www.twitch.tv/emilioblacksmith"
+					target="_blank"
+				>
+					
+				</a>
+				<a
+					href="mailto:emilioblacksmithlush@gmail.com"
+					target="_blank"
+				>
+					
+				</a>
+			</div>
+		</div>
+	);
 }
